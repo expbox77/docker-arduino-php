f0555192d95d7863cc1ccb4a8245ca68f30d297f
expbox77 <expbox77@gmail.com>
2022-07-21 01:21:06 +0900

    :D

--- run.sh
+++ run.sh
@@ -1,16 +1,27 @@
 #!/bin/bash
 
-docker-compose down
+# DOCKER_COMPOSE_RESULT=$(docker-compose ps)
+if [[ "$(docker-compose ps)" == *Up* ]]; then
+    echo "Docker-Compose Down"
+    docker-compose down
+fi
 
-DOCKER_NETWORK_RESULT=$(docker network ls)
-if [[ "$DOCKER_NETWORK_RESULT" != *arduino_net* ]]; then
+# DOCKER_NETWORK_RESULT=$(docker network ls)
+if [[ "$(docker network ls)" != *arduino_net* ]]; then
     echo "Create arduino_net Network"
     docker network create arduino_net
 fi
 
-echo -e "Build Docker-Compose"
-docker-compose build -q
+ENV_FILE=".env"
+if [ -e $ENV_FILE ]; then 
+    echo -e "Build Docker-Compose"
+    docker-compose build -q
 
-echo -e "Docker-Compose Service Start..."
-docker-compose up -d
+    echo -e "Docker-Compose Service Start..."
+    docker-compose up -d
+
+else
+    echo -e "Docker-Compose Service Do not Start. Check .env File"
+fi
+
 
